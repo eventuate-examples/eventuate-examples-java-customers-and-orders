c36fbaeea258d2f7a30b8c0444bc8a3635b17b19
Main <stanislav.semichevskiy@digitalarrowtech.com>
2016-08-08 23:31:04 +0300

    - docker-compose build fixes

--- customers-command-side-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/customers/commandsideservice/main/CustomersCommandSideService.java
+++ customers-command-side-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/customers/commandsideservice/main/CustomersCommandSideService.java
@@ -1,9 +1,10 @@
 package net.chrisrichardson.eventstore.examples.customersandorders.customers.commandsideservice.main;
 
+import net.chrisrichardson.eventstore.examples.customersandorders.customers.commandsideservice.CustomersCommandSideServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 
 public class CustomersCommandSideService {
     public static void main(String[] args) {
-        SpringApplication.run(CustomersCommandSideService.class, args);
+        SpringApplication.run(CustomersCommandSideServiceConfiguration.class, args);
     }
 }
