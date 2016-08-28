a4cce1c871205b2d9911df7a777790b7625b8c72
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-08-29 02:12:06 +0300

    - fixed order history service configuration

--- order-history-query-side-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/orders/history/querysideservice/OrderHistoryQuerySideServiceConfiguration.java
+++ order-history-query-side-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/orders/history/querysideservice/OrderHistoryQuerySideServiceConfiguration.java
@@ -2,14 +2,14 @@ package net.chrisrichardson.eventstore.examples.customersandorders.orders.histor
 
 import io.eventuate.javaclient.spring.httpstomp.EventuateHttpStompClientConfiguration;
 import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
-import net.chrisrichardson.eventstore.examples.customersandorders.views.orderhistory.OrderHistoryViewConfiguration;
+import net.chrisrichardson.eventstore.examples.customersandorders.orders.web.OrderHistoryWebConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({OrderHistoryViewConfiguration.class, EventuateHttpStompClientConfiguration.class, CommonSwaggerConfiguration.class})
+@Import({OrderHistoryWebConfiguration.class, EventuateHttpStompClientConfiguration.class, CommonSwaggerConfiguration.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class OrderHistoryQuerySideServiceConfiguration {
