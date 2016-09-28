1f3121358f28a483fa14a0eb1ba0d5ab1a444a72
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-09-28 18:09:32 +0300

    - fixed gradle build scripts

--- buildSrc/src/main/groovy/VerifyEventStoreEnvironmentPlugin.groovy
+++ buildSrc/src/main/groovy/VerifyEventStoreEnvironmentPlugin.groovy
@@ -6,8 +6,10 @@ class VerifyEventStoreEnvironmentPlugin implements Plugin<Project> {
        project.test {
             beforeSuite { x ->
                 if (x.parent == null) {
-                    if (System.getenv("EVENTUATE_API_KEY_ID") == null && System.getenv("EVENTUATE_API_KEY_SECRET") == null)
-                        logger.warn("\nPLEASE make sure that Eventuate-related environment variables EVENTUATE_API_KEY_ID and EVENTUATE_API_KEY_SECRET are set, see sample-set-remote-env.sh !!!!\n")
+                    if (!project.hasProperty("eventuateDriver") || project.property("eventuateDriver") != "local") {
+                        if (System.getenv("EVENTUATE_API_KEY_ID") == null && System.getenv("EVENTUATE_API_KEY_SECRET") == null)
+                            logger.warn("\nPLEASE make sure that Eventuate-related environment variables EVENTUATE_API_KEY_ID and EVENTUATE_API_KEY_SECRET are set, see sample-set-remote-env.sh !!!!\n")
+                    }
                 }
             }
         }
