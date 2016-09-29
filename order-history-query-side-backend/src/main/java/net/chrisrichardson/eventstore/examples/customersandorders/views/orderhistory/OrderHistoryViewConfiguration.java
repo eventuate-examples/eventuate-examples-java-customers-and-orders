c4cb945126002a9fdeb34a05f5bcfb9a3f5754f7
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-09-29 17:10:41 +0300

    -fixed OrderHistoryViewConfiguration

--- order-history-query-side-backend/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/views/orderhistory/OrderHistoryViewConfiguration.java
+++ order-history-query-side-backend/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/views/orderhistory/OrderHistoryViewConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableEventHandlers
 @Import(OrderHistoryViewMongoConfiguration.class)
 @EnableEventHandlers
 public class OrderHistoryViewConfiguration {
