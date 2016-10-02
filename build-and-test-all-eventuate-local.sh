7533bcebec5842391fd0dce1623cd28620a9eec0
Chris Richardson <chris@chrisrichardson.net>
2016-10-02 06:38:11 -0700

    Fixed build-and-test-all-*.sh issue

--- build-and-test-all-eventuate-local.sh
+++ build-and-test-all-eventuate-local.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 export EXTRA_INFRASTRUCTURE_SERVICES=cdcservice
+export EVENTUATE_LOCAL=yes
+
 ./_build-and-test-all.sh -f docker-compose-eventuate-local.yml -P eventuateDriver=local $*
