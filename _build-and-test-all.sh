e9e63d215d9d93305b41347182d2d08247b8528f
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-09-09 18:40:42 +0300

    docker image versions moved to gradle

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -12,18 +12,11 @@ if [ -z "$SPRING_DATA_MONGODB_URI" ] ; then
   echo Set SPRING_DATA_MONGODB_URI $SPRING_DATA_MONGODB_URI
 fi
 
-DOCKER_COMPOSE="docker-compose"
-
-while [ "$1" = "-f" ] ; do
-  shift;
-  DOCKER_COMPOSE="$DOCKER_COMPOSE -f ${1?}"
-  shift
-done
 
 if [ "$1" = "--use-existing" ] ; then
   shift;
 else
-  ${DOCKER_COMPOSE?} down
+  ./gradlew ${database}${mode}ComposeDown
 fi
 
 NO_RM=false
@@ -35,18 +28,17 @@ fi
 
 ./compile-contracts.sh
 
-
-./gradlew --stacktrace $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* testClasses
-
 if [ ! -z "$EXTRA_INFRASTRUCTURE_SERVICES" ]; then
-   ${DOCKER_COMPOSE?} up -d $EXTRA_INFRASTRUCTURE_SERVICES
+    ./gradlew ${EXTRA_INFRASTRUCTURE_SERVICES}ComposeBuild
+    ./gradlew ${EXTRA_INFRASTRUCTURE_SERVICES}ComposeUp
 fi
 
+
+./gradlew --stacktrace $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* testClasses
 ./gradlew --stacktrace $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* build -x :e2e-test:test
 
-${DOCKER_COMPOSE?} build
-
-${DOCKER_COMPOSE?} up -d
+./gradlew ${database}${mode}ComposeBuild
+./gradlew ${database}${mode}ComposeUp
 
 ./wait-for-services.sh $DOCKER_HOST_IP 8081 8082 8083
 
@@ -55,5 +47,5 @@ set -e
 ./gradlew -a $BUILD_AND_TEST_ALL_EXTRA_GRADLE_ARGS $* :e2e-test:cleanTest :e2e-test:test -P ignoreE2EFailures=false
 
 if [ $NO_RM = false ] ; then
-  ${DOCKER_COMPOSE?} down
+  ./gradlew ${database}${mode}ComposeDown
 fi
--- build-and-test-all-eventuate-local-mysql.sh
+++ build-and-test-all-eventuate-local-mysql.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-export EXTRA_INFRASTRUCTURE_SERVICES=cdcservice
 export EVENTUATE_LOCAL=yes
+export EXTRA_INFRASTRUCTURE_SERVICES=mysqlbinlogcdc
 
 . ./set-env-eventuate-local-mysql.sh
-./_build-and-test-all.sh -f docker-compose-eventuate-local-mysql.yml $BUILD_AND_TEST_ALL_EVENTUATE_LOCAL_EXTRA_COMPOSE_ARGS $* -P eventuateDriver=local
+
+export database=mysql
+export mode=binlog
+
+./_build-and-test-all.sh $* -P eventuateDriver=local
