cfd8724622638f9980653370ef7725ed79ce5f1f
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-03-03 15:02:37 +0300

    Updated libraries

--- customers-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/customersservice/CustomersServiceMain.java
+++ customers-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/customersservice/CustomersServiceMain.java
@@ -1,6 +1,6 @@
 package net.chrisrichardson.eventstore.examples.customersandorders.customersservice;
 
-import io.eventuate.javaclient.driver.EventuateDriverConfiguration;
+import io.eventuate.local.java.spring.javaclient.driver.EventuateDriverConfiguration;
 import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
 import net.chrisrichardson.eventstore.examples.customersandorders.customersservice.web.CustomerWebConfiguration;
 import org.springframework.boot.SpringApplication;
