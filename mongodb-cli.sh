b7c70d46a34f1aa48720215807546d40fdfd49d1
Chris Richardson <chris@chrisrichardson.net>
2020-10-24 16:10:32 -0700

    Upgraded mongo:3.0.4 -> mongo:3.6 to fix WSL2 issue

--- mongodb-cli.sh
+++ mongodb-cli.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run  $* --network=${PWD##*/}_default --rm  mongo:3.0.4 sh -c "exec /usr/bin/mongo --host mongodb customers_and_orders"
+docker run  $* --network=${PWD##*/}_default --rm  mongo:3.6 sh -c "exec /usr/bin/mongo --host mongodb customers_and_orders"
