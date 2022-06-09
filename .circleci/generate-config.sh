d83795e5550ff037067b6742dd8af0bdc53e18ac
Chris Richardson <chris@chrisrichardson.net>
2022-06-09 12:32:56 +0900

    Upgrade ORB, and compile-contracts first

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -3,7 +3,7 @@
 cat > generated_config.yml <<END
 version: 2.1
 orbs:
-  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.1.0"
+  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.2.1"
 workflows:
   version: 2.1
   build-test-and-deploy:
@@ -15,5 +15,7 @@ cat >> generated_config.yml <<END
       - eventuate-gradle-build-and-test/build-and-test:
           name: $build_script
           script: ./$build_script
+          maven_cache_command: ./compile-contracts.sh
+          checksum_files: 'gradle.properties gradle/wrapper/gradle-wrapper.properties ./common-contracts/pom.xml'
 END
 done
