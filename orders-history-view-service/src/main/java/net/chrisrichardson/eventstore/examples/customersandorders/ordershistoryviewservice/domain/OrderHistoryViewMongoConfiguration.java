9361f9b8ca88d144f86ec76fab77454e4ff45192
Chris Richardson <chris@chrisrichardson.net>
2023-09-12 15:05:19 -0700

    Retry on DuplicateKeyException during MongoDB

--- orders-history-view-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/ordershistoryviewservice/domain/OrderHistoryViewMongoConfiguration.java
+++ orders-history-view-service/src/main/java/net/chrisrichardson/eventstore/examples/customersandorders/ordershistoryviewservice/domain/OrderHistoryViewMongoConfiguration.java
@@ -3,14 +3,13 @@ package net.chrisrichardson.eventstore.examples.customersandorders.ordershistory
 import net.chrisrichardson.eventstore.examples.customersandorders.ordershistoryviewservice.service.OrderHistoryViewService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories
 public class OrderHistoryViewMongoConfiguration {
   @Bean
-  public OrderHistoryViewService orderHistoryViewService(CustomerViewRepository customerViewRepository, OrderViewRepository orderViewRepository, MongoTemplate mongoTemplate) {
-    return new OrderHistoryViewService(customerViewRepository, orderViewRepository, mongoTemplate);
+  public OrderHistoryViewService orderHistoryViewService(CustomerViewRepository customerViewRepository, OrderViewRepository orderViewRepository) {
+    return new OrderHistoryViewService(customerViewRepository, orderViewRepository);
   }
 }
